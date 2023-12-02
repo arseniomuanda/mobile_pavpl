501b387632b9240dc6ffc58821afb51d6dc7a8e4
Claudio Silva <claudio.dasilva@merkle.com>
2023-12-02 01:30:49 +0000

    fetching from fork

--- lib/widgets/data_grid_widget.dart
+++ lib/widgets/data_grid_widget.dart
@@ -1,5 +1,7 @@
 import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:mobile_pavpl/app/core/consts.dart';
 import 'package:syncfusion_flutter_datagrid/datagrid.dart';
 
 class DataGridWidget extends StatefulWidget {
@@ -21,8 +23,16 @@ class _DataGridWidgetState extends State<DataGridWidget> {
         Recluso(
           id: i + 1,
           name: _faker.person.name(),
-          entrada: DateTime.timestamp(),
-          saida: DateTime.timestamp()
+          entrada: DateFormat('dd-MM-yyyy hh:mm').parse(
+            formateDate.format(
+              DateTime.now(),
+            ),
+          ),
+          saida: DateFormat('dd-MM-yyyy hh:mm').parse(
+            formateDate.format(
+              DateTime.now(),
+            ),
+          ),
         ),
       );
     }
@@ -39,60 +49,60 @@ class _DataGridWidgetState extends State<DataGridWidget> {
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) => Column(
-    crossAxisAlignment: CrossAxisAlignment.center,
-    children: [
-      SizedBox(
-        height: constraints.maxHeight - 60,
-        width: constraints.maxWidth,
-        child: SfDataGrid(
-            allowSorting: true,
-            allowTriStateSorting: true,
-            showSortNumbers: true,
-            columnWidthMode: ColumnWidthMode.fill,
-            rowsPerPage: 20,
-            source: _employeeDataSource,
-            columns: [
-              GridColumn(
-                  columnName: 'id',
-                  label: const Center(
-                    child: Text(
-                      'ID',
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                  )),
-              GridColumn(
-                  columnName: 'name',
-                  label: const Center(
-                      child: Text('Name',
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: constraints.maxHeight - 60,
+            width: constraints.maxWidth,
+            child: SfDataGrid(
+                allowSorting: false,
+                allowTriStateSorting: false,
+                showSortNumbers: false,
+                columnWidthMode: ColumnWidthMode.fill,
+                rowsPerPage: 20,
+                source: _employeeDataSource,
+                columns: [
+                  GridColumn(
+                      columnName: 'id',
+                      label: const Center(
+                        child: Text(
+                          'ID',
                           overflow: TextOverflow.ellipsis,
-                          style: TextStyle(fontWeight: FontWeight.bold)))),
-              GridColumn(
-                  columnName: 'entrada',
-                  label: const Center(
-                    child: Text('Data Entrada',
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(fontWeight: FontWeight.bold)),
-                  )),
-              GridColumn(
-                  columnName: 'saida',
-                  label: const Center(
-                    child: Text('Data Saida',
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(fontWeight: FontWeight.bold)),
-                  )),
-            ]),
-      ),
-      SizedBox(
-          height: 60,
-          width: constraints.maxWidth,
-          child: SfDataPager(
-            delegate: _employeeDataSource,
-            pageCount: _reclusos.length / rowsPerPage,
-            visibleItemsCount: 3,
-            direction: Axis.horizontal,
-          ))
-    ],
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      )),
+                  GridColumn(
+                      columnName: 'name',
+                      label: const Center(
+                          child: Text('Name',
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(fontWeight: FontWeight.bold)))),
+                  GridColumn(
+                      columnName: 'entrada',
+                      label: const Center(
+                        child: Text('Data Entrada',
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(fontWeight: FontWeight.bold)),
+                      )),
+                  GridColumn(
+                      columnName: 'saida',
+                      label: const Center(
+                        child: Text('Data Saida',
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(fontWeight: FontWeight.bold)),
+                      )),
+                ]),
+          ),
+          SizedBox(
+              height: 60,
+              width: constraints.maxWidth,
+              child: SfDataPager(
+                delegate: _employeeDataSource,
+                pageCount: _reclusos.length / rowsPerPage,
+                visibleItemsCount: 3,
+                direction: Axis.horizontal,
+              ))
+        ],
       ),
     );
   }
@@ -104,8 +114,7 @@ class ReclusosDataSource extends DataGridSource {
         .map<DataGridRow>((e) => DataGridRow(cells: [
               DataGridCell<int>(columnName: 'id', value: e.id),
               DataGridCell<String>(columnName: 'name', value: e.name),
-              DataGridCell<DateTime>(
-                  columnName: 'entrada', value:  e.entrada),
+              DataGridCell<DateTime>(columnName: 'entrada', value: e.entrada),
               DataGridCell<DateTime>(columnName: 'saida', value: e.saida),
             ]))
         .toList();
